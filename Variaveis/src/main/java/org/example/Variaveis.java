e46589d65e07b991fece29117cfef837e64d09c1
Pedro H <xtron44@gmail.com>
2024-04-07 19:10:37 -0300

    Tipos de variáveis

---
+++ Variaveis/src/main/java/org/example/Variaveis.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Variaveis {
+    public static void main(String[] args) {
+        String name="Pedro Henrique";//variável do tipo String(object)
+        int age=20;//variável inteira
+        boolean married=true;//variável booleana
+        double number=500.55;//variável double
+        int[] notas={3,4,5,6,7,8,9,10};//array de int
+        String[] nomes=new String[5];//array de String
+        List<String> names=new ArrayList<>() ;//Lista de Strings
+        names.add(name);//adiciono uma String no ArrayList
+        final char GENDER='M';//Constante do tipo char
+
+    }
+}
